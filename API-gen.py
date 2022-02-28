559386cfaa77123b10a47f5a1d5ff6f46ef906a0
Mariana Meireles <mariana@psychonautgirl.space>
2022-02-27 23:07:26 -0800

    adding files

--- API-gen.py
+++ API-gen.py
@@ -9,7 +9,7 @@ def generate_string_docs(commands_list):
 
         opts = out.decode('utf-8')
 
-        with open('../../docs/source/user_guide/commands_micromamba/' + c + '.rst', 'w') as f:
+        with open('docs/source/user_guide/commands_micromamba/' + c + '.rst', 'w') as f:
             f.write('.. _commands_micromamba/' + c + ':' + '\n\n')
             f.write('``' + c + '``' + '\n')
             f.write('=' * (len(c) + 4) + '\n')
@@ -60,7 +60,7 @@ if __name__ == "__main__":
 
 
     # Generate sub-files for config command
-    with open('../../docs/source/user_guide/commands_micromamba/config.rst', 'a') as f:
+    with open('docs/source/user_guide/commands_micromamba/config.rst', 'a') as f:
         config_subcommands = ['**Subcommands:**\n\n',
                               '- :ref:`commands_micromamba/config/list`\n',
                               '- :ref:`commands_micromamba/config/sources`\n',
